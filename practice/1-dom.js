8ecef23ce710b3e82841363626377a58d66e5f2e
hari-uhk <hari.uggini@kellogg.northwestern.edu>
2021-02-04 19:04:36 -0600

    break 1

--- practice/1-dom.js
+++ practice/1-dom.js
@@ -9,6 +9,24 @@ window.addEventListener('DOMContentLoaded', function () {
   // 3. replace the element's inner html with new html using the dice values above
 
   // ⬇️ ⬇️ ⬇️ YOUR CODE GOES HERE ⬇️ ⬇️ ⬇️
+let dice1
+let dice2
+
+dice1 = getRandomInt(6)
+dice2 = getRandomInt(6)
+
+console.log(dice1)
+console.log(dice2)
+
+let dice = document.querySelector('.dice')
+// console.log(dice)
+
+dice.innerHTML = `
+        <img src="../images/dice/${dice1}.png" class="die w-1/2 md:w-1/6">
+        <img src="../images/dice/${dice2}.png" class="die w-1/2 md:w-1/6">
+
+`
+
 
   // ⬆️ ⬆️ ⬆️ YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE ⬆️ ⬆️ ⬆️
 })
